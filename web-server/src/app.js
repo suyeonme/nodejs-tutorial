5f66cad43dd71709e6a7f7f0244e64babdeb49c8
Suyeon Kang <tndusrkd92@gmail.com>
2021-02-26 15:40:27 +0900

    Create header and footer partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,10 +1,44 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+// Setup paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+app.set('views', path.join(__dirname, '../templates/views'));
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+// Render handlebars template(views)
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Suyeon',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Suyeon',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help section',
+    name: 'Suyeon',
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.');
