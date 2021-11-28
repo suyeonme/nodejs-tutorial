1074c15b09e901c55ee0aca2bcb583ccd17819d0
Suyeon Kang <tndusrkd92@gmail.com>
2021-11-28 20:13:26 +0900

    display a message using mustache.js

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,12 +1,33 @@
 const socket = io();
 
-socket.on('message', msg => console.log(msg));
-
+// Elements
 const form = document.querySelector('#message-form');
+const input = form.querySelector('input');
+const button = form.querySelector('button');
+const locationButton = document.querySelector('#send-location');
+const messages = document.querySelector('#messages');
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+
+socket.on('message', msg => {
+  const html = Mustache.render(messageTemplate, {
+    message: msg,
+  });
+  messages.insertAdjacentHTML('beforeend', html);
+});
+
 form.addEventListener('submit', e => {
   e.preventDefault();
+  button.setAttribute('disabled', 'disabled');
+
   const message = e.target.elements.message.value;
+
   socket.emit('sendMessage', message, error => {
+    button.removeAttribute('disabled');
+    input.value = '';
+    input.focus();
+
     if (error) {
       return console.log(error);
     }
@@ -14,11 +35,12 @@ form.addEventListener('submit', e => {
   });
 });
 
-const locationButton = document.querySelector('#send-location');
 locationButton.addEventListener('click', () => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by browser.');
   }
+
+  locationButton.setAttribute('disabled', 'disabled');
 
   navigator.geolocation.getCurrentPosition(position => {
     const coords = {
@@ -28,6 +50,7 @@ locationButton.addEventListener('click', () => {
 
     socket.emit('sendLocation', coords, () => {
       console.log('Location shared.');
+      locationButton.removeAttribute('disabled');
     });
   });
 });
